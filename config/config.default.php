de48c4469e6d72043f8f1fd1c57f988abe2481b1
Wolfgang Moritz <wolfmoritz@gmail.com>
2018-05-23 10:18:28 +0100

    Adding basic structure

---
+++ config/config.default.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Default Configuration Settings
+ *
+ * Define all instance specific settings in config.local.php.
+ */
+
+/**
+ * Production boolean variable controls debug and environment modes
+ */
+$config['production'] = true;
+
+/**
+ * Administration Email Address
+ */
+$config['user']['email'] = '';
+
+/**
+ * Database Settings
+ */
+$config['database']['host'] = 'localhost';
+$config['database']['dbname'] = '';
+$config['database']['username'] = '';
+$config['database']['password'] = '';
+
+/**
+ * Sessions
+ */
+$config['session']['cookieName'] = ''; // Name of the cookie
+$config['session']['checkIpAddress'] = true;
+$config['session']['checkUserAgent'] = true;
+$config['session']['salt'] = ''; // Salt key to hash
+$config['session']['secondsUntilExpiration'] = 7200;
---
+++ config/routes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Custom Public Application Routes
+ *
+ * Use example below to define custom static route
+ */
+
+// Example public static route
+// $app->get('/custom-static-route', function ($request, $response, $args) {
+//     return (new Piton\Controllers\ControllerName($this))->MethodName($request, $response, $args);
+// });
