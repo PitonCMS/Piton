a2c3d14a51d3c23e29687aa2c09bb4db0671316e
Wolfgang Moritz <wolfmoritz@gmail.com>
2018-05-24 22:49:17 +0100

    Changed delete confirm alert class. Added in button to add user rows to form.

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1,5 +1,13 @@
 // Delete confirm prompt
-$('body').on('click', '.deleteButton', function() {
+$('body').on('click', '.delete-confirm', function() {
   var reply = confirm('Are you sure you want to delete?');
   return reply;
+});
+
+// Add user row
+$('#user-emails .add-user-row').on('click', function() {
+    var $userRow = $(this).parent('#user-emails').find('div:last').clone();
+    $userRow.find('input').val('');
+    $userRow.find('a').attr('href', '#');
+    $(this).before($userRow);
 });
