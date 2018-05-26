57033a10a00d326e7fb45da7f3774582c7eff5f2
Wolfgang Moritz <wolfmoritz@gmail.com>
2018-05-26 16:25:06 +0100

    working on admin BS styles

--- public/assets/admin/js/admin.js
+++ public/assets/admin/js/admin.js
@@ -1,12 +1,12 @@
 // Delete confirm prompt
-$('body').on('click', '.delete-confirm', function() {
+$('body').on('click', '.jsDeleteConfirm', function() {
   var reply = confirm('Are you sure you want to delete?');
   return reply;
 });
 
 // Add user row
-$('#user-emails .add-user-row').on('click', function() {
-    var $userRow = $(this).parent('#user-emails').find('div:last').clone();
+$('.jsAddUserRow').on('click', function() {
+    var $userRow = $(this).parent('.jsUserForm').find('div:last').clone();
     $userRow.find('input').val('');
     $userRow.find('a').attr('href', '#');
     $(this).before($userRow);
