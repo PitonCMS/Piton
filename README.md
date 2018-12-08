bd17c736696bf9c9a5aa189abf2cf0b93b3af4cb
Wolfgang Moritz <wolfmoritz@gmail.com>
2018-12-08 22:20:08 +0000

    Updated default theme to work with new structure.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Piton
 Simple Content Management System
 
-To install run `composer create-project --ignore-platform-reqs pitoncms/piton pitontest "dev-master"`.
+To install `cd` to where you want to create a new project directory and run `composer create-project --ignore-platform-reqs pitoncms/piton my-project "dev-master"`.
+
+## While in Development Mode
+To get updates, use git to pull the latest piton composer.lock files, and then composer install to update the vendor packages:
+
+    git pull composer master
+    composer update
+
--- public/assets/admin/js/admin.js
+++ public/assets/admin/js/admin.js
@@ -1,3 +1,5 @@
+$('.datepicker').datepicker();
+
 // Delete confirm prompt
 $('body').on('click', '.jsDeleteConfirm', function() {
   var reply = confirm('Are you sure you want to delete?');
@@ -12,12 +14,72 @@ $('.jsAddUserRow').on('click', function() {
     $(this).before($userRow);
 });
 
-// Make sure element and section names are one clean word
-$('form input.jsRefNameValidate').on('blur', function() {
-    var elementName = $(this).val();
-    if (elementName.match(/[^a-zA-Z0-9_]/g)) {
-        elementName = elementName.replace(/[^a-zA-Z0-9_]/g, '_');
-        elementName = elementName.toLowerCase();
-        $(this).val(elementName);
+// Add Page Section Element
+$('.jsAddElement').on('click', function() {
+    var $sectionParent = $(this).parent('.jsSectionParent');
+    var elementType = $(this).data('element-type');
+    var sectionCodeName = $(this).data('section-code-name');
+
+    $.ajax({
+        url: '/admin/page/element/fetch',
+        method: "POST",
+        data: {
+            sectionCodeName: sectionCodeName,
+            elementType: elementType
+        },
+        success: function (r) {
+            var $newElement = $(r.html);
+            $newElement.appendTo($sectionParent);
+            $newElement.find('.jsMDE').each(function() {
+                simplemde = new SimpleMDE({
+                    element: this,
+                    forceSync: true
+                });
+            });
+        }
+    });
+});
+
+// Delete page section element
+$('.jsSectionParent').on('click', '.jsDeleteSectionElement', function () {
+    var sectionElementId = $(this).data('section-element-id') || 'x';
+    var physicalDelete = true;
+
+    if (!isNaN(sectionElementId)) {
+        $.ajax({
+            url: '/admin/page/section/deleteelement/' + sectionElementId,
+            method: "GET",
+            success: function (r) {
+                if (r.status != 'success') {
+                    physicalDelete = false;
+                }
+            }
+        });
+    }
+
+    // Remove element HTML
+    if (physicalDelete) {
+        $(this).parents('.jsElementParent').slideUp('normal', function () {
+            $(this).remove();
+        });
+    }
+});
+
+// Toggle element selector
+$('.jsElementType').on('click', 'input[type="radio"]', function() {
+    var selectedTypeOption = $(this).val();
+    $('.jsElementOptional.d-block').toggleClass('d-block d-none');
+
+    if (selectedTypeOption === 'media' || selectedTypeOption === 'hero') {
+        $(this).parents('.jsElementType').siblings('.jsElementMediaPath').toggleClass('d-none d-block');
+        return;
+    }
+    if (selectedTypeOption === 'mediaGroup') {
+        $(this).parents('.jsElementType').siblings('.jsElementMediaGroup').toggleClass('d-none d-block');
+        return;
+    }
+    if (selectedTypeOption === 'collection') {
+        $(this).parents('.jsElementType').siblings('.jsElementCollection').toggleClass('d-none d-block');
+        return;
     }
 });
