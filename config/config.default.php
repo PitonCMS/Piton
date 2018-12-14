e06e0b8369de6973367018938793999c94a91edf
Wolfgang Moritz <wolfmoritz@gmail.com>
2018-12-14 23:11:28 +0000

    Updated config file documentation.

--- config/config.default.php
+++ config/config.default.php
@@ -1,17 +1,20 @@
 <?php
 /**
  * Default Configuration Settings
+ * DO NOT CHANGE - Define all instance specific settings in config.local.php.
  *
- * Define all instance specific settings in config.local.php.
+ * Copy this file to config.local.php and set desired configuration settings.
  */
 
 /**
- * Production boolean variable controls debug and environment modes
+ * Production
+ * Boolean variable controls debug and environment modes
+ * Set to false in config.local.php on a development environment.
  */
 $config['production'] = true;
 
 /**
- * Database Settings
+ * Database
  */
 $config['database']['host'] = 'localhost';
 $config['database']['dbname'] = '';
@@ -21,7 +24,7 @@ $config['database']['password'] = '';
 /**
  * Sessions
  */
-$config['session']['cookieName'] = ''; // Name of the cookie
+$config['session']['cookieName'] = 'pitoncms'; // Name of the cookie
 $config['session']['checkIpAddress'] = true;
 $config['session']['checkUserAgent'] = true;
 $config['session']['salt'] = ''; // Salt key to hash
