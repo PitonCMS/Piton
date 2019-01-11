31598fe451a459c3ab2dd58cc332b00affb91ae6
Peri Moritz <peri.moritz@gmail.com>
2019-01-11 09:28:30 +0000

    Link to fancybox lightbox .video-wrapper for responsive video updated classes on images to work with fancybox added video sass file added responsive video javascript ignore .map files

--- public/assets/default/js/custom.js
+++ public/assets/default/js/custom.js
@@ -1,8 +1,44 @@
 // navbar hamburger toggler
 
-$('.menu-toggle').click(function() {
-  
+$('.menu-toggle').click(function () {
+
   $('.site-nav').toggleClass('site-nav--open', 500);
   $(this).toggleClass('open');
-  
+
 })
+
+// Find all YouTube videos
+var $allVideos = $("iframe[src^='https://www.youtube.com']");
+console.log($allVideos);
+    // The element that is fluid width
+    $fluidEl = $("body");
+
+// Figure out and save aspect ratio for each video
+$allVideos.each(function() {
+
+  $(this)
+    .data('aspectRatio', this.height / this.width)
+
+    // and remove the hard coded width/height
+    .removeAttr('height')
+    .removeAttr('width');
+
+});
+
+// When the window is resized
+$(window).resize(function() {
+
+  var newWidth = $fluidEl.width();
+
+  // Resize all videos according to their own aspect ratio
+  $allVideos.each(function() {
+
+    var $el = $(this);
+    $el
+      .width(newWidth)
+      .height(newWidth * $el.data('aspectRatio'));
+
+  });
+
+// Kick off one resize to fix all videos on page load
+}).resize();
