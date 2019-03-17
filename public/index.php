8ce7bba751d93d31f920ef4992aeaef1d81ae9b8
Wolfgang Moritz <wolfmoritz@gmail.com>
2019-03-17 09:36:52 +0000

    Changed max file upload to 4M in Docker build, and added PitonCMS block to index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * PitonCMS (https://github.com/PitonCMS)
+ *
+ * @link      https://github.com/PitonCMS/Piton
+ * @copyright Copyright (c) 2015 - 2019 Wolfgang Moritz
+ * @license   https://github.com/PitonCMS/Piton/blob/master/LICENSE (MIT License)
+ */
 
 // Application entry point for all requests
 
