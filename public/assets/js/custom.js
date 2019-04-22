8d632147b0443cc17730cc46c12b79e5cd97b51e
Wolfgang Moritz <wolfmoritz@gmail.com>
2019-04-22 13:03:05 +0100

    Updated contact element, and front end JS to submit contact form.

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,8 +1,24 @@
 // navbar hamburger toggler
-
 $('.menu-toggle').click(function () {
-
   $('.site-nav').toggleClass('site-nav--open', 500);
   $(this).toggleClass('open');
+});
 
-})
+// Contact form submission
+$("#contact-form").on('submit', function(e) {
+  e.preventDefault();
+  let $parentDiv = $(this).parent('div');
+  let postData = $(this).serialize();
+  $.ajax({
+    url: '/contact',
+    method: "POST",
+    data: postData,
+    success: function (r) {
+      $parentDiv.fadeOut().empty();
+      $parentDiv.append(r.response).fadeIn();
+    },
+    error: function(r) {
+      console.log('There was an error submitting the form. Contact your administrator.')
+    }
+  });
+});
