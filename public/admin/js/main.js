38f4d589e85f560f5c1dc61b4e910204a3afbac2
Wolfgang Moritz <wolfmoritz@gmail.com>
2019-04-28 21:28:09 +0100

    Revised message management in admin.

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -10,8 +10,8 @@ $('.jsDatePicker').datepicker({
 });
 
 // Delete confirm prompt
-var confirmDeletePrompt = function(msg) {
-    var message = msg || 'Are you sure you want to delete?';
+let confirmDeletePrompt = function(msg) {
+    let message = msg || 'Are you sure you want to delete?';
     return confirm(message);
 }
 
@@ -25,7 +25,7 @@ $('body').on('click', '.jsDeleteConfirm', function() {
 
 // Add user row
 $('.jsAddUserRow').on('click', function() {
-    var $userRow = $(this).parent('.jsUserForm').find('div:last').clone();
+    let $userRow = $(this).parent('.jsUserForm').find('div:last').clone();
     $userRow.find('input').val('');
     $userRow.find('a').attr('href', '#');
     $(this).before($userRow);
@@ -102,11 +102,11 @@ $('.jsBlockParent').on('click', '.jsDeleteBlockElement', function (e) {
     if (!confirmDeletePrompt('Are you sure you want to delete this element?')) {
         return;
     }
-    var blockElementId = $(this).data('element-id') || 'x';
-    var $element = $(this).parents('.jsElementParent');
+    let blockElementId = $(this).data('element-id') || 'x';
+    let $element = $(this).parents('.jsElementParent');
     let $blockParent = $(this).parents('.jsBlockParent');
     let elementLimit = $blockParent.find('.jsAddElement').data('element-count-limit') || 100;
-    var removeElement = function() {
+    let removeElement = function() {
         $element.slideUp('normal', function () {
             $element.remove();
         });
@@ -116,7 +116,7 @@ $('.jsBlockParent').on('click', '.jsDeleteBlockElement', function (e) {
             $blockParent.find('.jsAddElement').prop('disabled', false);
         }
     }
-    var postData = {id: blockElementId}
+    let postData = {id: blockElementId}
     postData[pitonConfig.csrfTokenName] = pitonConfig.csrfTokenValue;
 
     if (!isNaN(blockElementId)) {
@@ -141,7 +141,7 @@ $('.jsBlockParent').on('click', '.jsDeleteBlockElement', function (e) {
 
 // Toggle element selector
 $('.jsBlockParent').on('click', '.jsElementType input[type="radio"]', function() {
-    var selectedTypeOption = $(this).data('enable-input');
+    let selectedTypeOption = $(this).data('enable-input');
     $('.jsElementOption.d-block').toggleClass('d-block d-none');
 
     if (selectedTypeOption === 'image' || selectedTypeOption === 'hero') {
@@ -158,12 +158,14 @@ $('.jsBlockParent').on('click', '.jsElementType input[type="radio"]', function()
     }
 });
 
+// Clear media input and remove image from page editor
 $('.block-element-wrapper').on('change', 'input[name^=element_image_path]', function() {
     if ($(this).val() === '') {
     $(this).parents('.jsMediaInput').find('img').attr('src','');
     }
 });
 
+// Select media for page
 $('.block-element-wrapper').on('click', '.jsSelectMediaFile', function() {
     let $input = $(this).parents('.input-group').find('input');
     let $img = $(this).parents('.jsMediaInput').find('img');
@@ -179,6 +181,80 @@ $('.block-element-wrapper').on('click', '.jsSelectMediaFile', function() {
         method: "GET",
         success: function (r) {
             $('#mediaModal').find('.modal-body').html(r.html).end().modal();
+        }
+    });
+});
+
+// --------------------------------------------------------
+// Message management
+// --------------------------------------------------------
+// +/- Message count
+let changeMessageCount = (sign) => {
+    let count = parseInt($('.jsMessageCount').html() || 0);
+    if ('+' === sign) {
+        count++
+    } else {
+        if (--count == 0) count = null;
+    }
+    $('.jsMessageCount').html(count);
+}
+let removeMessage = ($message, sign) => {
+    $message.slideUp(() => {
+        $message.remove();
+    });
+    changeMessageCount(sign);
+}
+let messageStates = {
+    read: {
+        button: "Mark as Unread",
+        class: "btn-outline-primary",
+        isRead: "Y"
+    },
+    unread: {
+        button: "Mark as Read",
+        class: "btn-primary",
+        isRead: "N"
+    }
+}
+$('.jsAllMessagesWrap').on('click', 'button', (e) => {
+    e.preventDefault();
+    let request = $(e.target).attr('value');
+    if ('delete' === request && !confirmDeletePrompt()) {
+        return false;
+    }
+    let isRead = $(e.target).data('isRead');
+    let $message = $(e.target).parents('.jsMessageWrap');
+    // jQuery ignores the button value, so append that to post data
+    let postData = $message.find('form').serialize() + '&button=' + encodeURI($(e.target).attr('value'));
+    $.ajax({
+        url: '/admin/message/save',
+        method: "POST",
+        data: postData,
+        success: (r) => {
+            console.log(r)
+            if (r.status === "success" && 'delete' === request) {
+                removeMessage($message);
+            } else if (r.status === "success" && "toggle" === request) {
+                $clonedMessage = $message.clone();
+                if (isRead === 'Y') {
+                    $clonedMessage.find('button[value=toggle]')
+                        .data('isRead', messageStates.unread.isRead)
+                        .html(messageStates.unread.button)
+                        .toggleClass(messageStates.read.class+' '+messageStates.unread.class);
+                    $('.jsUnreadMessages').prepend($clonedMessage);
+                    removeMessage($message, '+');
+                } else {
+                    $clonedMessage.find('button[value=toggle]')
+                        .data('isRead', messageStates.read.isRead)
+                        .html(messageStates.read.button)
+                        .toggleClass(messageStates.unread.class+' '+messageStates.read.class);
+                        $('.jsReadMessages').prepend($clonedMessage);
+                        removeMessage($message);
+                }
+            }
+        },
+        error: (r) => {
+            console.log('There was an error submitting the form. Contact your administrator.')
         }
     });
 });
