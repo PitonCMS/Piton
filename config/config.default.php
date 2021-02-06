0e4a27a2ab6e68eddb1bdfc7fc657cf5e2619932
Wolfgang Moritz <wolfmoritz@gmail.com>
2021-02-06 17:38:02 +0000

    Removed all security headers from .htaccess, and moved into config.default.php to load dynamically at runtime. Also enabled expires caching by type for css, js, html. Updated contact.html element to include CSRF key and token. Removed _pitonCongfigJs.html and moved contact route into front.js. Added page.title to title element. Added nonces to script elements. Removed analytics settings, which should be hardcoded into the base layout if used.

--- config/config.default.php
+++ config/config.default.php
@@ -75,3 +75,31 @@ $config['email']['smtpPort'] = '';
  * Affects both administration and front end pagination.
  */
 $config['pagination']['resultsPerPage'] = 20;
+
+/**
+ * Response Headers
+ *
+ * Sets response headers dynamically at application runtime
+ * Define any standard or custom header as a key:value, under the ['header'] array
+ *
+ * Notes
+ * - Strict-Transport-Security is *only* set when not on localhost to avoid forcing future requests to https
+ * - Content-Security-Policy can be either a simple string, a bool false to disable CSP, or a sub-array of directives and values
+ *      - See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy for more information
+ *      - See https://csp.withgoogle.com/docs/strict-csp.html for the default policy explanation below
+ *      - To include a nonce in the directive, include the string 'nonce' (quoted) in the value and this will be expanded to a unique base64 nonce
+ *      - Consider adding a report URI to send CSP violations to a central log
+ */
+$config['header']['X-Frame-Options'] = 'DENY';
+$config['header']['X-Content-Type-Options'] = 'nosniff';
+$config['header']['Referrer-Policy'] = 'no-referrer-when-downgrade';
+$config['header']['Feature-Policy'] = 'self';
+$config['header']['X-XSS-Protection'] = '1; mode=block';
+$config['header']['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains';
+$config['header']['Content-Security-Policy']['default-src'] = "'self'";
+$config['header']['Content-Security-Policy']['script-src'] = "'self' 'nonce' 'unsafe-inline' 'strict-dynamic'";
+$config['header']['Content-Security-Policy']['style-src'] = "'self' 'unsafe-inline' https://fonts.gstatic.com https://fonts.googleapis.com";
+$config['header']['Content-Security-Policy']['font-src'] = "'self' https://fonts.gstatic.com";
+$config['header']['Content-Security-Policy']['img-src'] = "*";
+$config['header']['Content-Security-Policy']['base-uri'] = "'none'";
+// $config['header']['Content-Security-Policy']['report-uri'] = "";
